9bc74dc09e6fff2e69b51113b690a66bf1b9ac46
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-01-22 18:45:18 -0500

    Append protocol to uri's if their not present

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,11 +1,12 @@
 class Bookmark < ActiveRecord::Base
   validates :title, :uri, presence: true
-
+  
   belongs_to :user
 
   has_many :taggings
-  has_many :tags, :through => :taggings
+  has_many :tags, :through => :taggings, dependent: :destroy
 
+  before_create :add_url_protocol
 
   def tag_names=(names)
   	tag_names = names.split(",").collect{|s| s.strip.downcase}.uniq
@@ -15,5 +16,13 @@ class Bookmark < ActiveRecord::Base
 
   def tag_names
   	self.tags.map(&:name).join(",")
+  end
+
+  protected
+
+  def add_url_protocol
+    unless self.uri[/\Ahttp:\/\//] || self.uri[/\Ahttps:\/\//]
+      self.uri = "http://#{self.uri}"
+    end
   end
 end
