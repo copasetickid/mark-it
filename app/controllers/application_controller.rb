fb967ef70ff196575eeb5f5b5476d1bdf56d780d
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-01-21 21:56:30 -0500

    Bootstrap js and override sign-in & sign-out paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #Overwriting the sign_in redirect path method
+  def after_sign_in_path_for(resource)
+    bookmarks_path
+  end
+
+  #Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  #Overriding devise method :authenticate_user!
+  def authenticate_user!
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
+
+  #Overwriting the sign_in redirect path method
+  def after_sign_up_path_for(resource)
+    bookmarks_path
+  end
 end
