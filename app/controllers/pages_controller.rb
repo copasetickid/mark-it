ec56002a4d8ff4687bf1e9e0574e6b4e811439ae
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-01-22 05:33:37 -0500

    Redirects logged in users to view bookmarks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+
   def home
+    redirect_to bookmarks_path if user_signed_in?
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,11 +3,23 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
 
   describe "GET #home" do
-  	context "Vistors & Users" do
+  	context "Users" do
 	    it "returns http success" do
 	      get :home
 	      expect(response).to have_http_status(:success)
 	    end
 	  end
+
+    context "Authenticated users" do
+      let(:user) { create(:user) }
+
+      before do
+        sign_in user
+      end
+      it "redirects users to the Bookmarks page" do
+        get :home
+        expect(response).to redirect_to bookmarks_path
+      end
+    end
   end
 end
