d0613074904534e2090bb5bbc975deb97f8e794b
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-01-22 12:45:52 -0500

    Feature specs for creating a bookmark

---
+++ spec/features/bookmarks/creating_bookmarks_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.feature "Users can create new bookmarks" do
+	before do
+		login_as(create(:user))
+		visit bookmarks_path
+		click_link "Add"
+	end
+
+	scenario "with valid attributes" do
+		fill_in "Title", with: "Sublime Text 3"
+		fill_in "Url", with: "http://www.sublimetext.com/3"
+		click_button "Create Bookmark"
+
+		expect(page).to have_content "Bookmark has been created."
+		expect(page).to have_content "Sublime Text 3"
+	end
+
+	scenario 'when providing invalid attributes' do
+		click_button "Create Bookmark"
+
+		expect(page).to have_content "Bookmark has not been created."
+		expect(page).to have_content "Title can't be blank"
+	end
+end
